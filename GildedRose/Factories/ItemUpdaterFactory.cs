1acd2752f1f3456b7bd9dd3f6e0c43b30e14327b
H4S0 - <hasanyenidogdu@gmail.com>
2026-07-22 23:54:52 +0200

    Implement base factory.

--- GildedRose/Factories/ItemUpdaterFactory.cs
+++ GildedRose/Factories/ItemUpdaterFactory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GildedRoseKata.Base;
+using GildedRoseKata.Models;
+using GildedRoseKata.Updaters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,34 @@ namespace GildedRoseKata.Factories
 {
     internal class ItemUpdaterFactory
     {
+        private const string AgedBrie = "Aged Brie";
+        private const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string ConjuredManaCake = "Conjured Mana Cake";
+
+        public static ItemUpdaterBase Create(Item item)
+        {
+            if (item.Name == AgedBrie)
+            {
+                return new AgedBrieUpdater();
+            }
+
+            if (item.Name == BackstagePasses)
+            {
+                return new BackstagePassUpdater();
+            }
+
+            if (item.Name == Sulfuras)
+            {
+                return new SulfurasUpdater();
+            }
+
+            if (item.Name == ConjuredManaCake)
+            {
+                return new ConjuredItemUpdater();
+            }
+
+            return new StandardItemUpdater();
+        }
     }
 }
